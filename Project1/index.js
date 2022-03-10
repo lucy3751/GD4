b38d5c6a740d2bd88858b1352c7d5162be235183
lucy3751 <78235983+lucy3751@users.noreply.github.com>
2022-03-10 14:51:03 -0500

    update the comments so they make more sense and can refer back to next time

--- Project1/index.js
+++ Project1/index.js
@@ -1,6 +1,7 @@
-//From: https://www.w3schools.com/howto/howto_js_draggable.asp and https://www.youtube.com/watch?v=8zcfIu3tyw4 
+//used this tutorial https://www.w3schools.com/howto/howto_js_draggable.asp for the dragging interaction
 
 
+// call dragElement for all divs (ids go by "mydiv<number>")
 for (i=1; i <=25; i++){
     dragElement(document.getElementById(`mydiv${i}`));
 }
@@ -15,13 +16,13 @@ function dragElement(elmnt) {
         e = e || window.event;
         e.preventDefault();
 
-        // when mouse is not pressed:
+        // when mouse is not pressed, call closeDragElement:
         document.onmouseup = closeDragElement;
 
-        // call a function whenever the cursor moves:
+        // when mouse is moving, call elementDrag
         document.onmousemove = elementDrag;
 
-        // the active div popups (top layer)        
+        // the popup that the mouse clicks will be the topmost layer        
         for (i=1; i <=25; i++){
             document.getElementById(`mydiv${i}`).style.zIndex = 1;
         }
@@ -29,52 +30,54 @@ function dragElement(elmnt) {
     }
 
     function elementDrag(e) {
-        console.log("hello")
 
-        e = e || window.event;
-        e.preventDefault();
+        //events
+            e = e || window.event;
+            e.preventDefault();
 
-        //get the left and top value of the popup div <-- this gives a string instead of a number, which we have to convert it to a number (integer) using parseInt
-        let elmntLeft = parseInt(getComputedStyle(elmnt).getPropertyValue("left"));
-        let elmntTop = parseInt(getComputedStyle(elmnt).getPropertyValue("top"));
+        // used this tutorial https://www.youtube.com/watch?v=8zcfIu3tyw4 for a simplier way of updating the positions of the div 
 
-        //update the popup div position/css (with .style)
-        //current left/top value + distance the mouseX/Y moved (from the last event)
-        elmnt.style.left = `${elmntLeft + e.movementX}px`;
-        elmnt.style.top = `${elmntTop + e.movementY}px`;
+            //get the left and top value of the popup div <-- this gives a string instead of a number, which we have to convert it to a number (integer) using parseInt
+            let elmntLeft = parseInt(getComputedStyle(elmnt).getPropertyValue("left"));
+            let elmntTop = parseInt(getComputedStyle(elmnt).getPropertyValue("top"));
+
+            //update the popup div position/css (with .style)
+            //current left/top value + distance the mouseX/Y moved (from the last event)
+            elmnt.style.left = `${elmntLeft + e.movementX}px`;
+            elmnt.style.top = `${elmntTop + e.movementY}px`;
         
 
-        let divHeight = parseInt(getComputedStyle(elmnt).getPropertyValue("height"));
-        let divTopPoint = parseInt(elmnt.style.top);
+        // restrict the popup so that it cannot move out of the window/viewport
+            let divHeight = parseInt(getComputedStyle(elmnt).getPropertyValue("height"));
+            let divTopPoint = parseInt(elmnt.style.top);
 
-        let divWidth = parseInt(getComputedStyle(elmnt).getPropertyValue("width"));
-        let divLeftPoint = parseInt(elmnt.style.left);
+            let divWidth = parseInt(getComputedStyle(elmnt).getPropertyValue("width"));
+            let divLeftPoint = parseInt(elmnt.style.left);
 
-        let windowHeight = window.innerHeight;
-        let windowWidth = window.innerWidth;
+            let windowHeight = window.innerHeight;
+            let windowWidth = window.innerWidth;
 
+            if(divTopPoint<0){
+                elmnt.style.top = `0px`;
+            }
 
-        // restrict the popup so that it cannot move out of the window
-        if(divTopPoint<0){
-            elmnt.style.top = `0px`;
-        }
+            // if(divTopPoint+divHeight > windowHeight){
+            //     elmnt.style.top = `${windowHeight-divHeight}px`;
+            // }
 
-        // if(divTopPoint+divHeight > windowHeight){
-        //     elmnt.style.top = `${windowHeight-divHeight}px`;
-        // }
-
-        if(divLeftPoint<0){
-            elmnt.style.left = `0px`;
-        }
-        if(divLeftPoint+divWidth > windowWidth){
-            elmnt.style.left = `${windowWidth-divWidth}px`;
-        }
+            if(divLeftPoint<0){
+                elmnt.style.left = `0px`;
+            }
+            if(divLeftPoint+divWidth > windowWidth){
+                elmnt.style.left = `${windowWidth-divWidth}px`;
+            }
 
     }
 
     function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null;
+        // document.onmouseup = null;
+
+        /* set mouse to not moving */
         document.onmousemove = null;
     }
 }
